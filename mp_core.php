c61510e706475e5a2b13225ef62928f0fb0ca822
Vít Michalek <info@semor.cz>
2021-08-19 14:33:49 +0200

    init

---
+++ mp_core.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class MP{
+	static $token = "";
+	static function send(){
+		
+	}
+	static function addDomain($domain,$category = array()){
+		
+	}
+	static function changeCategory($domain,$category = array()){
+		
+	}
+	static function getStats($domain,$from="1970-01-01",$to = ""){
+		
+	}
+}
+
+?>
