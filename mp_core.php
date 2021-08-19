9d0fae7f7292bbdffba49f235540eac8cece2e7b
Vít Michalek <info@semor.cz>
2021-08-19 14:39:44 +0200

    update 1

--- mp_core.php
+++ mp_core.php
@@ -3,17 +3,36 @@
 
 class MP{
 	static $token = "";
-	static function send(){
+	static function send($url,$data){
+		$data["token"] = MP::$token;
 		
+
+		$cURLConnection = curl_init('https://mujparking.cz/api/v2');
+		curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+		$apiResponse = curl_exec($cURLConnection);
+		curl_close($cURLConnection);
+		return $apiResponse;
 	}
 	static function addDomain($domain,$category = array()){
-		
+		$data = array();
+		$data["domain"] = $domain;
+		$data["category"] = $category;		
+		return MP::send("/?addDomain",$data);
 	}
 	static function changeCategory($domain,$category = array()){
-		
+		$data = array();
+		$data["domain"] = $domain;
+		$data["category"] = $category;		
+		return MP::send("/?changeCategory",$data);
 	}
 	static function getStats($domain,$from="1970-01-01",$to = ""){
-		
+		$data = array();
+		$data["domain"] = $domain;
+		$data["from"] = $from;
+		$data["to"] = $to;
+		return MP::send("/?getStats",$data);
 	}
 }
 
