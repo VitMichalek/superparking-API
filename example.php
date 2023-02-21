55b0ee20722d127a4354c3ddcdd45b8dd8678d58
Vít Michalek <info@semor.cz>
2023-02-21 12:09:23 +0100

    Update example.php

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 include_once("mp_core.php");
 
 
-MP::$token = "muj tajny token";
+SP::$token = "muj tajny token";
 
 //add new domain
 /*
@@ -11,10 +11,10 @@ parametry:
 2) kategorie - pole povolených kategorii - pokud nebude určeno zařadí se automaticky jako ostatní  seznam kategorii MP::getCategoryList();
 3) typ - (F -formulář na podej, R - přesměrování přeprodej)
 */
-$id = MP::addDomain("example.com",array("CD","CY"),"R");
+$id = SP::addDomain("example.com",array("CD","CY"),"R");
 
 //changeCategory
-MP::addDomain("example.com",array("ZZ"),"F");
+SP::addDomain("example.com",array("ZZ"),"F");
 
 
 //get stats
@@ -24,14 +24,14 @@ MP::addDomain("example.com",array("ZZ"),"F");
 3) datum do
 4) group  - day, month
 */
-MP::getStats("example.com","2021-08-01","2021-08-19","day");
+SP::getStats("example.com","2021-08-01","2021-08-19","day");
 
 //vybere vše za celou dobu co je domena v systemu
-MP::getStats("example.com","","","day");
+SP::getStats("example.com","","","day");
 
 //list your domain
-MP::getDomainList();
+SP::getDomainList();
 
 //smazaní domeny - logicky
-MP::delDomain("example.com");
+SP::delDomain("example.com");
 ?>
